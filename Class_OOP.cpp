67ba03487830a5a66857f9ae344b29fc595fc4ec
kizaru <rizkiramada128@gmail.com>
2023-04-12 14:46:03 +0700

    memanggil methode yang telah dibuat kedalam program utama

--- Class_OOP.cpp
+++ Class_OOP.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 using namespace std;
+
+
 class mahasiswa {
 private:
 	int nim;
 	string nama;
-private:
+public:
 	void inputdata() {
 		cout << "masukan NIM	:";
 		cin >> nim;
 		cout << "masukan nama	:";
 		cin >> nama;
 	}
-	void inputdata() {
-		cout << "masukan NIM	:";
-		cin >> nim;
-		cout << "masukan nama	:";
-		cin >> nama;
+	void tampildata() {
+		cout << "\nNIM	:" << nim << endl;
+		cout << "nama	:" << nama << endl;
 	}
-
 };
 
 class Matakuliah {
+private:
+	string kode, namamk;
+	int sks;
+
+public:
 
 };
 
 int main() {
+	mahasiswa mhs;
+
+	mhs.inputdata();
+	mhs.tampildata();
 }
